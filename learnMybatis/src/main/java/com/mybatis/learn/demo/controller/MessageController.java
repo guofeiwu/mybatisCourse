0af8b5d2b235402c977fcc8948af5b6938edbf1d
wuguofei <guofei_wu@163.com>
2017-08-14 18:02:18 +0800

    消除了几个空格

--- learnMybatis/src/main/java/com/mybatis/learn/demo/controller/MessageController.java
+++ learnMybatis/src/main/java/com/mybatis/learn/demo/controller/MessageController.java
@@ -1,13 +1,12 @@
 package com.mybatis.learn.demo.controller;
 
 
+import com.mybatis.learn.demo.model.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/messages")
 public class MessageController {
-
-
 
 }
