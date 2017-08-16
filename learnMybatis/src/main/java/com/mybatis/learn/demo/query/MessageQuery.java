51bcea006f500f2f483df2ee7a647b7412120cf3
wuguofei <guofei_wu@163.com>
2017-08-16 23:21:51 +0800

    1.参数的处理（单个参数，多个参数（使用param注解），map类型的参数，list类型的参数，） 2.关联查询，返回类型，级联查询，association,collection的使用

--- learnMybatis/src/main/java/com/mybatis/learn/demo/query/MessageQuery.java
+++ learnMybatis/src/main/java/com/mybatis/learn/demo/query/MessageQuery.java
@@ -2,6 +2,9 @@ package com.mybatis.learn.demo.query;
 
 
 import com.mybatis.learn.demo.model.Message;
+import com.mybatis.learn.demo.model.Person;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,4 +15,12 @@ public interface MessageQuery {
     List<Map> queryAll();
     Map queryOneById(int id);
     Message getMessageById(int id);
+    Message getMessageByCommandAndId(@Param("id") Integer id, @Param("command") String command);
+    Message getMessageByMap(Map map);
+    Message getMessageByList(List<Integer> ids);
+    List<Message> getMessageList(String description);
+    @MapKey("id")
+    Map getMessageReturnLikeDescription(String description);
+    //Map<Integer,Message> getMessageReturnLikeDescription(String description);
+    Person getPerson(Integer pid);
 }
